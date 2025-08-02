2131e100031635fdd3e90a0b665474f9d1eb9271
sideeqhammed <sideeqhammed05.com>
2025-08-02 19:40:26 +0100

    Added Form-Creation-Validation

---
+++ fetch-data.js
@@ -0,0 +1,35 @@
+async function fetchUserData()  {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const users = await response.json();
+    // const users = users_arr.map(user => user.name)
+    // console.log(users)
+    dataContainer.innerHTML = ""
+
+    let userList = "<ul>"
+    users.forEach(user => {
+      userList += `<li>${user.name}</li>`
+    });
+    userList += "</ul>"
+    console.log(userList);
+    dataContainer.innerHTML = userList;
+
+    // for (const key in users) {
+    //   const value = users[key]
+    //   dataContainer.innerHTML += `<li>${value}</li>`
+    // }
+
+  } catch (err) {
+    dataContainer.innerHTML = ""
+    dataContainer.innerHTML = `Failed to load user data.`
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (fetchUserData))
+// fetchUserData()
---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const user_name = document.getElementById("username");
+    const username = user_name.value.trim();
+    // console.log(name);
+
+    const e_mail = document.getElementById("email");
+    const email = e_mail.value.trim();
+
+    const pass_word = document.getElementById("password");
+    const password = pass_word.value.trim();
+
+    let is_valid = true;
+    let messages = [];
+
+    if (username.length < 3) {
+      is_valid = false;
+      messages.push("Please use a valid username");
+    }
+
+    if (!(email.includes("@") && email.includes("."))){
+      is_valid = false;
+      messages.push("Please use a valid email");
+    }
+
+    if (password.length < 8) {
+      is_valid = false;
+      messages.push("Please use a valid password")
+    }
+
+    feedbackDiv.style.display = "block";
+
+    if (is_valid === true) {
+      feedbackDiv.innerHTML = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+    }else {
+      feedbackDiv.innerHTML = messages.join("</br>");
+      feedbackDiv.style.color = "#dc3545"
+    }
+
+    console.log(messages)
+  })
+})
